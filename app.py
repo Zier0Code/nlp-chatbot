fdfea36fff431e4ee5711bcdd197707f6317799f
Zier0Code <momoxsana01@gmail.com>
2024-10-20 21:54:06 +0800

    Added catch to intents 0

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+import subprocess
 
 from Chatbot import pred_class, get_response, words, classes, data_filtered
 
@@ -13,9 +14,10 @@ def predict():
         return jsonify({"answer": "No message provided"}), 400
 
     ints = pred_class(msg, words, classes)
-    res = {"response": get_response(ints,data_filtered),
-           "message": msg,
-           "tag": ints[0]}
+    if not ints:
+        res = {"response": "Sorry! I don’t understand., Please Try asking about STI ONLY.", "message": msg, "tag": "unknown"}
+    else:
+        res = {"response": get_response(ints, data_filtered), "message": msg, "tag": ints[0]}
     return jsonify(res)
 
 print(classes)
